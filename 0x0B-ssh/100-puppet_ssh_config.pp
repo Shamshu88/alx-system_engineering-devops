13ba5b5ad60041b656b8dc18ca62aa31d19c804b
Shamshu88 <rogersemaduk@gmail.com>
2023-06-27 19:08:56 -0700

    0x0B-ssh

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Using puppet to make changes to configuration files
+
+file { 'etc/ssh/ssh_config':
+	ensure => present,
+
+content =>"
+	#SSH client configuration file
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+	"
+}
